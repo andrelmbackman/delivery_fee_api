614fdc1320ada42afa71a7482eab3427a929ca93
Andre home <andrebackmann@gmail.com>
2024-01-23 15:48:25 +0200

    reformatted according to ruff and mypy

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from datetime import datetime
 import re
 import math
 from error_messages import (
-	INVALID_CART_VALUE,
-	INVALID_DELIVERY_DISTANCE,
-	INVALID_NUMBER_OF_ITEMS,
-	INVALID_TIME_FORMAT
+    INVALID_CART_VALUE,
+    INVALID_DELIVERY_DISTANCE,
+    INVALID_NUMBER_OF_ITEMS,
+    INVALID_TIME_FORMAT,
 )
 
 app = FastAPI(title="Delivery Fee API")
@@ -30,79 +30,86 @@ ITEMS_BULK_FEE = 120
 MIN_CART_VALUE = 1
 MIN_DELIVERY_DISTANCE = 0
 MIN_NUMBER_OF_ITEMS = 1
-TIME_FORMAT_REGEX = r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z'
+TIME_FORMAT_REGEX = r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z"
 TIME_FORMAT_DATETIME = "%Y-%m-%dT%H:%M:%SZ"
+
 
 # Class representing an order, post requests must follow this format.
 # By default, extra fields are not forbidden, but disregarded.
 class Order(BaseModel):
-	cart_value: int
-	delivery_distance: int
-	number_of_items: int
-	time: str
+    cart_value: int
+    delivery_distance: int
+    number_of_items: int
+    time: str
+
 
 # Validates that the request body is correctly formatted, and returns (an)
 # appropriate error description(s) where applicable.
-def	validate_order_data(order_data: Order = Body(...)):
-	error_messages = []
-	if order_data.cart_value < MIN_CART_VALUE:
-		error_messages.append(INVALID_CART_VALUE)
-	if order_data.delivery_distance < MIN_DELIVERY_DISTANCE:
-		error_messages.append(INVALID_DELIVERY_DISTANCE)
-	if order_data.number_of_items < MIN_NUMBER_OF_ITEMS:
-		error_messages.append(INVALID_NUMBER_OF_ITEMS)
-	if not re.match(TIME_FORMAT_REGEX, order_data.time):
-		error_messages.append(INVALID_TIME_FORMAT)
-	if error_messages:
-		raise HTTPException(status_code=400, detail=", ".join(error_messages))
-	return order_data
+def validate_order_data(order_data: Order = Body(...)):
+    error_messages = []
+    if order_data.cart_value < MIN_CART_VALUE:
+        error_messages.append(INVALID_CART_VALUE)
+    if order_data.delivery_distance < MIN_DELIVERY_DISTANCE:
+        error_messages.append(INVALID_DELIVERY_DISTANCE)
+    if order_data.number_of_items < MIN_NUMBER_OF_ITEMS:
+        error_messages.append(INVALID_NUMBER_OF_ITEMS)
+    if not re.match(TIME_FORMAT_REGEX, order_data.time):
+        error_messages.append(INVALID_TIME_FORMAT)
+    if error_messages:
+        raise HTTPException(status_code=400, detail=", ".join(error_messages))
+    return order_data
+
 
 # Calculates the surcharge depending on the distance of the order
 def distance_surcharge(distance: int) -> int:
-	if distance <= STARTING_DISTANCE:
-		return DISTANCE_STARTING_FEE
-	half_kms_started = math.ceil((distance - STARTING_DISTANCE) / 500)
-	return DISTANCE_STARTING_FEE + (DISTANCE_HALF_KM_FEE * half_kms_started)
+    if distance <= STARTING_DISTANCE:
+        return DISTANCE_STARTING_FEE
+    half_kms_started = math.ceil((distance - STARTING_DISTANCE) / 500)
+    return DISTANCE_STARTING_FEE + (DISTANCE_HALF_KM_FEE * half_kms_started)
+
 
 # Calculates the possible surcharge and bulk fee depending on
 # the number of items
 def items_surcharge(items: int) -> int:
-	fee = 0
-	if items > MAX_ITEMS_NO_SURCHARGE:
-		fee = ADDITIONAL_FEE_PER_ITEM * (items - MAX_ITEMS_NO_SURCHARGE)
-	if items > ITEMS_BULK_NUMBER:
-		fee += ITEMS_BULK_FEE
-	return fee
+    fee = 0
+    if items > MAX_ITEMS_NO_SURCHARGE:
+        fee = ADDITIONAL_FEE_PER_ITEM * (items - MAX_ITEMS_NO_SURCHARGE)
+    if items > ITEMS_BULK_NUMBER:
+        fee += ITEMS_BULK_FEE
+    return fee
+
 
 # Returns true if the order was placed during rush hour (Friday 3-7 PM)
 def is_rush_hour(time: str) -> bool:
-	try:
-		order_time = datetime.strptime(time, TIME_FORMAT_DATETIME)
-		if order_time.weekday() == 4:
-			return 15 <= order_time.hour < 19
-		else:
-			return False
-	except ValueError:
-		return False
+    try:
+        order_time = datetime.strptime(time, TIME_FORMAT_DATETIME)
+        if order_time.weekday() == 4:
+            return 15 <= order_time.hour < 19
+        else:
+            return False
+    except ValueError:
+        return False
+
 
 # Calculates the full delivery fee according to the instructions
 def calculate_delivery_fee(order_data: Order) -> float:
-	fee: float = 0
-	# free delivery on orders over a certain value.
-	if order_data.cart_value >= FREE_DELIVERY_CART_VALUE:
-		return 0
-	if order_data.cart_value < NO_SURCHARGE_MIN_CART_VALUE:
-		fee = NO_SURCHARGE_MIN_CART_VALUE - order_data.cart_value
-	fee += distance_surcharge(order_data.delivery_distance)
-	fee += items_surcharge(order_data.number_of_items)
-	if is_rush_hour(order_data.time):
-		fee = fee * RUSH_HOUR_MULTIPLIER
-	# the delivery fee cannot exceed this.
-	if fee > MAX_DELIVERY_FEE:
-		return MAX_DELIVERY_FEE
-	return fee
+    fee: float = 0
+    # free delivery on orders over a certain value.
+    if order_data.cart_value >= FREE_DELIVERY_CART_VALUE:
+        return 0
+    if order_data.cart_value < NO_SURCHARGE_MIN_CART_VALUE:
+        fee = NO_SURCHARGE_MIN_CART_VALUE - order_data.cart_value
+    fee += distance_surcharge(order_data.delivery_distance)
+    fee += items_surcharge(order_data.number_of_items)
+    if is_rush_hour(order_data.time):
+        fee = fee * RUSH_HOUR_MULTIPLIER
+    # the delivery fee cannot exceed this.
+    if fee > MAX_DELIVERY_FEE:
+        return MAX_DELIVERY_FEE
+    return fee
+
 
 @app.post("/delivery_fee")
 def fee_calculator(order_data: Order = Depends(validate_order_data)):
-	fee: int = int(calculate_delivery_fee(order_data))
-	return {"delivery_fee": fee}
+    fee: int = int(calculate_delivery_fee(order_data))
+    return {"delivery_fee": fee}
--- tests/delivery_fee_test.py
+++ tests/delivery_fee_test.py
@@ -3,257 +3,273 @@ import pytest
 import math
 from main import app
 from error_messages import (
-	INVALID_CART_VALUE,
-	INVALID_DELIVERY_DISTANCE,
-	INVALID_NUMBER_OF_ITEMS,
-	INVALID_TIME_FORMAT
+    INVALID_CART_VALUE,
+    INVALID_DELIVERY_DISTANCE,
+    INVALID_NUMBER_OF_ITEMS,
+    INVALID_TIME_FORMAT,
 )
 
 API_ENDPOINT = "/delivery_fee"
 
+
 def test_empty_request_body():
-	with TestClient(app) as client:
-		response = client.post(API_ENDPOINT)
-		assert response.status_code == 422
+    with TestClient(app) as client:
+        response = client.post(API_ENDPOINT)
+        assert response.status_code == 422
+
 
 def test_invalid_request_body():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 790,
-			"delivery_distance": 2235
-		}
-		response = client.post(API_ENDPOINT, json=data)
-		assert response.status_code == 422
+    with TestClient(app) as client:
+        data = {"cart_value": 790, "delivery_distance": 2235}
+        response = client.post(API_ENDPOINT, json=data)
+        assert response.status_code == 422
+
 
 def test_invalid_keys():
-	with TestClient(app) as client:
-		data = {
-			"value": 790,
-			"distance": 2235,
-			"items": 4,
-			"time": "2024-01-15T13:00:00Z"
-		}
-		response = client.post(API_ENDPOINT, json=data)
-		assert response.status_code == 422
+    with TestClient(app) as client:
+        data = {
+            "value": 790,
+            "distance": 2235,
+            "items": 4,
+            "time": "2024-01-15T13:00:00Z",
+        }
+        response = client.post(API_ENDPOINT, json=data)
+        assert response.status_code == 422
+
 
 def test_invalid_distance():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 790,
-			"delivery_distance": -2235,
-			"number_of_items": 4,
-			"time": "2024-01-26T16:00:00Z"
-		}
-		response = client.post(API_ENDPOINT, json=data)
-		assert response.status_code == 400
-		assert INVALID_DELIVERY_DISTANCE in response.json()["detail"]
+    with TestClient(app) as client:
+        data = {
+            "cart_value": 790,
+            "delivery_distance": -2235,
+            "number_of_items": 4,
+            "time": "2024-01-26T16:00:00Z",
+        }
+        response = client.post(API_ENDPOINT, json=data)
+        assert response.status_code == 400
+        assert INVALID_DELIVERY_DISTANCE in response.json()["detail"]
+
 
 def test_invalid_items():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 790,
-			"delivery_distance": 2235,
-			"number_of_items": 0,
-			"time": "2024-01-26T16:00:00Z"
-		}
-		response = client.post(API_ENDPOINT, json=data)
-		assert response.status_code == 400
-		assert INVALID_NUMBER_OF_ITEMS in response.json()["detail"]
+    with TestClient(app) as client:
+        data = {
+            "cart_value": 790,
+            "delivery_distance": 2235,
+            "number_of_items": 0,
+            "time": "2024-01-26T16:00:00Z",
+        }
+        response = client.post(API_ENDPOINT, json=data)
+        assert response.status_code == 400
+        assert INVALID_NUMBER_OF_ITEMS in response.json()["detail"]
+
 
 def test_invalid_time_formats():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 790,
-			"delivery_distance": 2235,
-			"number_of_items": 4,
-			"time": "2020-0-15Ö00:00:00Q"
-		}
-		response = client.post(API_ENDPOINT, json=data)
-		assert response.status_code == 400
-		assert INVALID_TIME_FORMAT in response.json()["detail"]
-		data = {
-			"cart_value": 790,
-			"delivery_distance": 2235,
-			"number_of_items": 4,
-			"time": "0-01-15T13:00:00"
-		}
-		response = client.post(API_ENDPOINT, json=data)
-		assert response.status_code == 400
-		assert INVALID_TIME_FORMAT in response.json()["detail"]
-		data = {
-			"cart_value": 790,
-			"delivery_distance": 2235,
-			"number_of_items": 4,
-			"time": "2023-x-y5T13:00:00"
-		}
-		response = client.post(API_ENDPOINT, json=data)
-		assert response.status_code == 400
-		assert INVALID_TIME_FORMAT in response.json()["detail"]
+    with TestClient(app) as client:
+        data = {
+            "cart_value": 790,
+            "delivery_distance": 2235,
+            "number_of_items": 4,
+            "time": "2020-0-15Ö00:00:00Q",
+        }
+        response = client.post(API_ENDPOINT, json=data)
+        assert response.status_code == 400
+        assert INVALID_TIME_FORMAT in response.json()["detail"]
+        data = {
+            "cart_value": 790,
+            "delivery_distance": 2235,
+            "number_of_items": 4,
+            "time": "0-01-15T13:00:00",
+        }
+        response = client.post(API_ENDPOINT, json=data)
+        assert response.status_code == 400
+        assert INVALID_TIME_FORMAT in response.json()["detail"]
+        data = {
+            "cart_value": 790,
+            "delivery_distance": 2235,
+            "number_of_items": 4,
+            "time": "2023-x-y5T13:00:00",
+        }
+        response = client.post(API_ENDPOINT, json=data)
+        assert response.status_code == 400
+        assert INVALID_TIME_FORMAT in response.json()["detail"]
+
 
 def test_all_invalid():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": -1,
-			"delivery_distance": -1,
-			"number_of_items": -4,
-			"time": "2020-0-15Ö00:00:00Q"
-		}
-		response = client.post(API_ENDPOINT, json=data)
-		assert response.status_code == 400
-		assert INVALID_CART_VALUE in response.json()["detail"]
-		assert INVALID_DELIVERY_DISTANCE in response.json()["detail"]
-		assert INVALID_NUMBER_OF_ITEMS in response.json()["detail"]
-		assert INVALID_TIME_FORMAT in response.json()["detail"]
+    with TestClient(app) as client:
+        data = {
+            "cart_value": -1,
+            "delivery_distance": -1,
+            "number_of_items": -4,
+            "time": "2020-0-15Ö00:00:00Q",
+        }
+        response = client.post(API_ENDPOINT, json=data)
+        assert response.status_code == 400
+        assert INVALID_CART_VALUE in response.json()["detail"]
+        assert INVALID_DELIVERY_DISTANCE in response.json()["detail"]
+        assert INVALID_NUMBER_OF_ITEMS in response.json()["detail"]
+        assert INVALID_TIME_FORMAT in response.json()["detail"]
 
 
 def test_valid_request():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 790,
-			"delivery_distance": 2235,
-			"number_of_items": 4,
-			"time": "2024-01-15T13:00:00Z"
-		}
-		expected_response = {"delivery_fee": 710}
-		response = client.post(API_ENDPOINT, json=data)
-	assert response.status_code == 200
-	assert response.json() == expected_response
+    with TestClient(app) as client:
+        data = {
+            "cart_value": 790,
+            "delivery_distance": 2235,
+            "number_of_items": 4,
+            "time": "2024-01-15T13:00:00Z",
+        }
+        expected_response = {"delivery_fee": 710}
+        response = client.post(API_ENDPOINT, json=data)
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
 
 def test_rush_hour():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 790,
-			"delivery_distance": 2235,
-			"number_of_items": 4,
-			"time": "2024-01-26T16:00:00Z"
-		}
-		expected_response = {"delivery_fee": 852}
-		response = client.post(API_ENDPOINT, json=data)
-	assert response.status_code == 200
-	assert response.json() == expected_response
+    with TestClient(app) as client:
+        data = {
+            "cart_value": 790,
+            "delivery_distance": 2235,
+            "number_of_items": 4,
+            "time": "2024-01-26T16:00:00Z",
+        }
+        expected_response = {"delivery_fee": 852}
+        response = client.post(API_ENDPOINT, json=data)
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
 
 def test_rush_hour_minimum_distance():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 1000,
-			"delivery_distance": 500,
-			"number_of_items": 4,
-			"time": "2024-01-26T16:00:00Z"
-		}
-		expected_response = {"delivery_fee": 240}
-		response = client.post(API_ENDPOINT, json=data)
-	assert response.status_code == 200
-	assert response.json() == expected_response
+    with TestClient(app) as client:
+        data = {
+            "cart_value": 1000,
+            "delivery_distance": 500,
+            "number_of_items": 4,
+            "time": "2024-01-26T16:00:00Z",
+        }
+        expected_response = {"delivery_fee": 240}
+        response = client.post(API_ENDPOINT, json=data)
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
 
 def test_rush_hour_minimum_distance_5_items():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 1000,
-			"delivery_distance": 500,
-			"number_of_items": 5,
-			"time": "2024-01-26T16:00:00Z"
-		}
-		expected_response = {"delivery_fee": 300}
-		response = client.post(API_ENDPOINT, json=data)
-	assert response.status_code == 200
-	assert response.json() == expected_response
+    with TestClient(app) as client:
+        data = {
+            "cart_value": 1000,
+            "delivery_distance": 500,
+            "number_of_items": 5,
+            "time": "2024-01-26T16:00:00Z",
+        }
+        expected_response = {"delivery_fee": 300}
+        response = client.post(API_ENDPOINT, json=data)
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
 
 def test_valid_cheap_order():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 1000,
-			"delivery_distance": 999,
-			"number_of_items": 4,
-			"time": "2024-01-15T13:00:00Z"
-		}
-		expected_response = {"delivery_fee": 200}
-		response = client.post(API_ENDPOINT, json=data)
-	assert response.status_code == 200
-	assert response.json() == expected_response
+    with TestClient(app) as client:
+        data = {
+            "cart_value": 1000,
+            "delivery_distance": 999,
+            "number_of_items": 4,
+            "time": "2024-01-15T13:00:00Z",
+        }
+        expected_response = {"delivery_fee": 200}
+        response = client.post(API_ENDPOINT, json=data)
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
 
 def test_free_delivery():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 20000,
-			"delivery_distance": 500,
-			"number_of_items": 1,
-			"time": "2024-01-15T13:00:00Z"
-		}
-		expected_response = {"delivery_fee": 0}
-		response = client.post(API_ENDPOINT, json=data)
-		assert response.status_code == 200
-		assert response.json() == expected_response
+    with TestClient(app) as client:
+        data = {
+            "cart_value": 20000,
+            "delivery_distance": 500,
+            "number_of_items": 1,
+            "time": "2024-01-15T13:00:00Z",
+        }
+        expected_response = {"delivery_fee": 0}
+        response = client.post(API_ENDPOINT, json=data)
+        assert response.status_code == 200
+        assert response.json() == expected_response
+
 
 def test_max_delivery_fee():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 100,
-			"delivery_distance": 50000,
-			"number_of_items": 100,
-			"time": "2024-01-15T13:00:00Z"
-		}
-		expected_response = {"delivery_fee": 1500}
-		response = client.post(API_ENDPOINT, json=data)
-		assert response.status_code == 200
-		assert response.json() == expected_response
+    with TestClient(app) as client:
+        data = {
+            "cart_value": 100,
+            "delivery_distance": 50000,
+            "number_of_items": 100,
+            "time": "2024-01-15T13:00:00Z",
+        }
+        expected_response = {"delivery_fee": 1500}
+        response = client.post(API_ENDPOINT, json=data)
+        assert response.status_code == 200
+        assert response.json() == expected_response
+
 
 def test_small_value_big_distance():
-	with TestClient(app) as client:
-		data = {
-			"cart_value": 10,
-			"delivery_distance": 20000,
-			"number_of_items": 4,
-			"time": "2024-01-23T23:00:00Z"
-		} # Should NOT be 4990
-		expected_response = {"delivery_fee": 1500}
-		response = client.post(API_ENDPOINT, json=data)
-		assert response.status_code == 200
-		assert response.json() == expected_response
+    with TestClient(app) as client:
+        data = {
+            "cart_value": 10,
+            "delivery_distance": 20000,
+            "number_of_items": 4,
+            "time": "2024-01-23T23:00:00Z",
+        }  # Should NOT be 4990
+        expected_response = {"delivery_fee": 1500}
+        response = client.post(API_ENDPOINT, json=data)
+        assert response.status_code == 200
+        assert response.json() == expected_response
+
 
 # Test all cart values ranging from 10-1000 in intervals of 10.
 @pytest.mark.parametrize("value", range(10, 1001, 10))
 def test_cart_values(value):
-	client = TestClient(app)
-	data = {
-		"cart_value": value,
-		"delivery_distance": 200,
-		"number_of_items": 4,
-		"time": "2024-01-23T23:00:00Z"
-	}
-	expected_fee = 1000 - value + 200
-	expected_response = {"delivery_fee": expected_fee}
-	response = client.post(API_ENDPOINT, json=data)
-	assert response.status_code == 200
-	assert response.json() == expected_response
+    client = TestClient(app)
+    data = {
+        "cart_value": value,
+        "delivery_distance": 200,
+        "number_of_items": 4,
+        "time": "2024-01-23T23:00:00Z",
+    }
+    expected_fee = 1000 - value + 200
+    expected_response = {"delivery_fee": expected_fee}
+    response = client.post(API_ENDPOINT, json=data)
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
 
 # Test full range of delivery distance fee.
 @pytest.mark.parametrize("distance", range(501, 7002, 500))
 def test_delivery_distance(distance):
-	client = TestClient(app)
-	data = {
-		"cart_value": 1000,
-		"delivery_distance": distance,
-		"number_of_items": 4,
-		"time": "2024-01-23T23:00:00Z"
-	}
-	expected_fee = (100 * math.ceil(distance / 500)) # starts at 200, no need to add starting fee
-	expected_response = {"delivery_fee": expected_fee}
-	response = client.post(API_ENDPOINT, json=data)
-	assert response.status_code == 200
-	assert response.json() == expected_response
+    client = TestClient(app)
+    data = {
+        "cart_value": 1000,
+        "delivery_distance": distance,
+        "number_of_items": 4,
+        "time": "2024-01-23T23:00:00Z",
+    }
+    expected_fee = 100 * math.ceil(
+        distance / 500
+    )  # starts at 200, no need to add starting fee
+    expected_response = {"delivery_fee": expected_fee}
+    response = client.post(API_ENDPOINT, json=data)
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
 
 # Test the full range of the items surcharge.
 @pytest.mark.parametrize("items", range(5, 26, 1))
 def test_number_of_items(items):
-	client = TestClient(app)
-	data = {
-		"cart_value": 1000,
-		"delivery_distance": 1000,
-		"number_of_items": items,
-		"time": "2024-01-23T23:00:00Z"
-	}
-	expected_fee = min(200 + ((items - 4) * 50) + (120 if items > 12 else 0), 1500)
-	expected_response = {"delivery_fee": expected_fee}
-	response = client.post(API_ENDPOINT, json=data)
-	assert response.status_code == 200
-	assert response.json() == expected_response
+    client = TestClient(app)
+    data = {
+        "cart_value": 1000,
+        "delivery_distance": 1000,
+        "number_of_items": items,
+        "time": "2024-01-23T23:00:00Z",
+    }
+    expected_fee = min(200 + ((items - 4) * 50) + (120 if items > 12 else 0), 1500)
+    expected_response = {"delivery_fee": expected_fee}
+    response = client.post(API_ENDPOINT, json=data)
+    assert response.status_code == 200
+    assert response.json() == expected_response
