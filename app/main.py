11a6ea2f4b6f891dc90a9bdc9464ef4d07db1ca1
Andre home <andrebackmann@gmail.com>
2024-01-26 14:02:45 +0200

    update comment format to docstrings

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
-from fastapi import FastAPI, Depends, HTTPException, Body
+from fastapi import FastAPI, Depends
 from datetime import datetime, timezone
 from dateutil import parser
 import math
 from app.models import Order
+from app.validation import validate_order_data
 import app.constants as const
-import app.error_messages as err
 
 app = FastAPI(title="Delivery Fee API")
 
 
-# Calculates the full delivery fee according to the instructions.
 def calculate_delivery_fee(order_data: Order) -> float:
+    """
+    Calculate the full delivery fee of the order.
+    """
     fee: float = 0
-    # Free delivery on orders over a certain value.
     if order_data.cart_value >= const.FREE_DELIVERY_CART_VALUE:
         return 0
     if order_data.cart_value < const.NO_SURCHARGE_MIN_CART_VALUE:
@@ -21,22 +22,25 @@ def calculate_delivery_fee(order_data: Order) -> float:
     fee += items_surcharge(order_data.number_of_items)
     if is_rush_hour(order_data.time):
         fee = fee * const.RUSH_HOUR_MULTIPLIER
-    # The delivery fee cannot exceed this.
-    if fee > const.MAX_DELIVERY_FEE:
+    if fee > const.MAX_DELIVERY_FEE: # The delivery fee cannot exceed this.
         return const.MAX_DELIVERY_FEE
     return fee
 
 
-# Calculates the surcharge depending on the distance of the order.
 def distance_surcharge(distance: int) -> int:
+    """
+    Calculate the surcharge depending on the delivery distance of the order.
+    """
     if distance <= const.STARTING_DISTANCE:
         return const.DISTANCE_STARTING_FEE
     half_kms_started = math.ceil((distance - const.STARTING_DISTANCE) / 500)
     return const.DISTANCE_STARTING_FEE + (const.DISTANCE_HALF_KM_FEE * half_kms_started)
 
 
-# Calculates the possible surcharge and bulk fee depending on the number of items.
 def items_surcharge(items: int) -> int:
+    """
+    Calculate the possible surcharge and bulk fee depending on the number of items.
+    """
     fee = 0
     if items > const.MAX_ITEMS_NO_SURCHARGE:
         fee = const.ADDITIONAL_FEE_PER_ITEM * (items - const.MAX_ITEMS_NO_SURCHARGE)
@@ -45,37 +49,18 @@ def items_surcharge(items: int) -> int:
     return fee
 
 
-# Returns true if the order was placed during rush hour (Friday 3-7 PM) UTC.
 def is_rush_hour(time: str) -> bool:
+    """
+    Determine whether or not the order was placed during rush hour (Friday 3-7PM UTC),
+    timezone offset considered. Exception should never be raised, since the field has
+    been validated, but we don't want to add a rush hour fee if anything unexpected happens.
+    """
     try:
         order_time: datetime = parser.isoparse(time)
-        print(order_time)
         order_time_utc = order_time.astimezone(timezone.utc)
         return order_time_utc.weekday() == 4 and 15 <= order_time_utc.hour < 19
-    # This should never happen, parsed in validate_order_data
     except Exception:
-        print("DOES NOT COMPUTE DATETIME")
         return False
-
-
-# Validates that the request body is correctly formatted, and returns (an)
-# appropriate error description(s) where applicable.
-def validate_order_data(order_data: Order = Body(...)):
-    error_messages = []
-    if order_data.cart_value < const.MIN_CART_VALUE:
-        error_messages.append(err.INVALID_CART_VALUE)
-    if order_data.delivery_distance < const.MIN_DELIVERY_DISTANCE:
-        error_messages.append(err.INVALID_DELIVERY_DISTANCE)
-    if order_data.number_of_items < const.MIN_NUMBER_OF_ITEMS:
-        error_messages.append(err.INVALID_NUMBER_OF_ITEMS)
-    try:
-        print(parser.isoparse(order_data.time))
-        parser.isoparse(order_data.time)
-    except Exception as e:
-        error_messages.append(err.INVALID_TIME_FORMAT + str(e))
-    if error_messages:
-        raise HTTPException(status_code=400, detail=", ".join(error_messages))
-    return order_data
 
 
 @app.post("/delivery_fee")
