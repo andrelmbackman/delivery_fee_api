767ce2aad573ba62e8fb9e411ec15a223ef28879
Andre home <andrebackmann@gmail.com>
2024-01-27 15:48:33 +0200

    update error message in case of passing string instead of int

--- app/error_messages.py
+++ app/error_messages.py
@@ -1,2 +1,3 @@
 INVALID_TIME_FORMAT = "Invalid time format: "
 INVALID_UTC_OFFSET = "Time string does not include timezone offset or 'Z'"
+INT_NOT_STRING = "Value must be an integer, not a string"
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from pydantic import BaseModel, PositiveInt, NonNegativeInt, field_validator
 from dateutil import parser
-from app.error_messages import INVALID_TIME_FORMAT, INVALID_UTC_OFFSET
+import app.error_messages as error_messages
 
 class Order(BaseModel):
     """
@@ -9,6 +9,12 @@ class Order(BaseModel):
     By default, extra fields are not forbidden, but disregarded. Pydantic handles the
     input validation by utilizing PositiveInt and NonNegativeInt data types.
     Model utilizes field_validator to ensure that the value is not a coerced string.
+
+    Attributes:
+        cart_value (NonNegativeInt): The value of the shopping cart in cents.
+        delivery_distance (NonNegativeInt): The distance between the store and customer's location in meters.
+        number_of_items (PositiveInt): The number of items in the customer's shopping cart.
+        time (str): Order time in ISO format.
     """
     cart_value: NonNegativeInt
     delivery_distance: NonNegativeInt
@@ -21,7 +27,7 @@ class Order(BaseModel):
     @classmethod
     def validate_not_string(cls, value):
         if isinstance(value, str):
-            raise ValueError("Value must be a positive integer, not a string")
+            raise HTTPException(status_code=400, detail=error_messages.INT_NOT_STRING)
         return value
 
     @field_validator("time")
@@ -31,7 +37,7 @@ class Order(BaseModel):
         try:
             parsed_time = parser.isoparse(time)
             if parsed_time.tzinfo is None and time[-1] != "Z":
-                raise HTTPException(status_code=400, detail=INVALID_UTC_OFFSET)
+                raise HTTPException(status_code=400, detail=error_messages.INVALID_UTC_OFFSET)
         except Exception as e:
-            raise HTTPException(status_code=400, detail=(INVALID_TIME_FORMAT + str(e)))
+            raise HTTPException(status_code=400, detail=(error_messages.INVALID_TIME_FORMAT + str(e)))
         return time
