96a1f19c8c57e6638a09672226f0e85445400d24
Andre home <andrebackmann@gmail.com>
2024-01-30 16:29:11 +0200

    add docstrings to main.py

--- app/main.py
+++ app/main.py
@@ -8,5 +8,19 @@ app = FastAPI(title="Delivery Fee API")
 
 @app.post("/delivery_fee")
 def fee_calculator(order_data: Order) -> DeliveryFeeResponse:
+    """
+    Calculate the delivery fee based on the provided order data.
+
+    Args:
+        order_data (Order): The order details including cart value, delivery distance, number of items, and order time.
+
+    Returns:
+        DeliveryFeeResponse: An object containing the calculated delivery fee in cents.
+
+    Description:
+    This endpoint calculates the delivery fee for an order based on the provided order data. 
+    The fee is computed using the calculate_delivery_fee function from delivery_fee.py.
+    The calculated fee is then returned as part of a DeliveryFeeResponse object.
+    """
     fee: int = calculate_delivery_fee(order_data)
     return DeliveryFeeResponse(delivery_fee=fee)
