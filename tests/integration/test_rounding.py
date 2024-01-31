82fbae86700a2a31d74d5a66906c610ce0c99da1
Andre home <andrebackmann@gmail.com>
2024-01-31 12:14:56 +0200

    update docstrings, change order

--- tests/integration/test_rounding.py
+++ tests/integration/test_rounding.py
@@ -9,53 +9,9 @@ Tests that ensure that rounding is handled properly. Due to the nature of the
 rush hour multiplier, only decimals of .2, .4, .6, and .8 are generated.
 """
 
-def test_round_up_dot_eight():
-    """Test if values resulting in a .8 fractional gets rounded up"""
-    with TestClient(app) as client:
-        payload = {
-            "cart_value": 996,
-            "delivery_distance": 500,
-            "number_of_items": 4,
-            "time": "2024-01-26T17:00:00Z",
-        }
-        expected_response = {"delivery_fee": 245} # 244,8
-        response = client.post(API_ENDPOINT, json=payload)
-        assert response.status_code == status.HTTP_200_OK
-        assert response.json() == expected_response
-
-
-def test_round_up_dot_six():
-    """Test if values resulting in a .6 fractional gets rounded up"""
-    with TestClient(app) as client:
-        payload = {
-            "cart_value": 997,
-            "delivery_distance": 500,
-            "number_of_items": 4,
-            "time": "2024-01-26T17:00:00Z",
-        }
-        expected_response = {"delivery_fee": 244} # 243,6
-        response = client.post(API_ENDPOINT, json=payload)
-        assert response.status_code == status.HTTP_200_OK
-        assert response.json() == expected_response
-
-
-def test_round_down_dot_four():
-    """Test if a .4 fractional gets rounded down"""
-    with TestClient(app) as client:
-        payload = {
-            "cart_value": 998,
-            "delivery_distance": 500,
-            "number_of_items": 4,
-            "time": "2024-01-26T17:00:00Z",
-        }
-        expected_response = {"delivery_fee": 242} # 242,4
-        response = client.post(API_ENDPOINT, json=payload)
-        assert response.status_code == status.HTTP_200_OK
-        assert response.json() == expected_response
-
 
 def test_round_down_dot_two():
-    """Test if a .2 fractional gets rounded down"""
+    """Test rounding down for a fractional value ending in .2"""
     with TestClient(app) as client:
         payload = {
             "cart_value": 999,
@@ -63,7 +19,53 @@ def test_round_down_dot_two():
             "number_of_items": 4,
             "time": "2024-01-26T17:00:00Z",
         }
-        expected_response = {"delivery_fee": 241} # 241,2
+        expected_response = {"delivery_fee": 241} # 241.2
+        response = client.post(API_ENDPOINT, json=payload)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == expected_response
+
+
+def test_round_down_dot_four():
+    """Test rounding down for a fractional value ending in .4"""
+
+    with TestClient(app) as client:
+        payload = {
+            "cart_value": 998,
+            "delivery_distance": 500,
+            "number_of_items": 4,
+            "time": "2024-01-26T17:00:00Z",
+        }
+        expected_response = {"delivery_fee": 242} # 242.4
+        response = client.post(API_ENDPOINT, json=payload)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == expected_response
+
+
+def test_round_up_dot_six():
+    """Test rounding up for a fractional value ending in .6"""
+    with TestClient(app) as client:
+        payload = {
+            "cart_value": 997,
+            "delivery_distance": 500,
+            "number_of_items": 4,
+            "time": "2024-01-26T17:00:00Z",
+        }
+        expected_response = {"delivery_fee": 244} # 243.6
+        response = client.post(API_ENDPOINT, json=payload)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == expected_response
+
+
+def test_round_up_dot_eight():
+    """"Test rounding up for a fractional value ending in .8"""
+    with TestClient(app) as client:
+        payload = {
+            "cart_value": 996,
+            "delivery_distance": 500,
+            "number_of_items": 4,
+            "time": "2024-01-26T17:00:00Z",
+        }
+        expected_response = {"delivery_fee": 245} # 244.8
         response = client.post(API_ENDPOINT, json=payload)
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == expected_response
